c95541fa6382f187746124ed8893e21f68413e38
Manuel <m.a.c.p90@gmail.com>
2018-08-04 14:56:01 -0400

    setting  environment

---
+++ reactApi/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+import axios from "axios";
+
+import "./App.css";
+
+class App extends Component {
+  getallTheTask() {}
+  render() {
+    return <h1>hellooooooo</h1>;
+    <button
+      onClick={() => {
+        this.getallTheTask();
+      }}
+    >
+      gey the tasl
+    </button>;
+  }
+}
+
+export default App;
